caaa6f76876c190be4fcd61d5341d2d89b374b25
范帅 <2931798419@qq.com>
2025-02-22 18:14:40 +0800

    fix 404样式修改

--- src/views/errorPage/404.vue
+++ src/views/errorPage/404.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>
-    <el-empty description="description" />
+  <div class="notspace">
+    <el-empty>
+      <template #image>
+        <img style="width: 256px" src="/icons/提示.svg" alt="" />
+      </template>
+      <template #description>
+        <div class="prompt">404</div>
+      </template>
+    </el-empty>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.notspace {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+.prompt {
+  font-family: PingFangSC-Regular;
+  font-size: 34px;
+  color: #666666;
+  letter-spacing: 0;
+  text-align: center;
+  line-height: 40px;
+  font-weight: 400;
+  margin-bottom: 24px;
+}
+:deep(.el-empty__image) {
+  width: 256px;
+}
+:deep(.el-button--primary) {
+  background: #de3a05;
+  border-radius: 4px;
+  border: 0;
+}
+</style>
